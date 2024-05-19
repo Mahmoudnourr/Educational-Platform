ee323ce60ce4978d0a572893b71cff35ba8c6276
mahmoudnourr <graphh37@gmail.com>
2024-05-20 00:25:38 +0300

    Adding Project2

--- EducationalPlatform/obj/Debug/net8.0/EducationalPlatform.AssemblyInfo.cs
+++ EducationalPlatform/obj/Debug/net8.0/EducationalPlatform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EducationalPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d8adabe2be7d83fc9270d7cfd85e1e9bf68aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d094809fa3117725f41160fd19172e108451abe7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EducationalPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EducationalPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
